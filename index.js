fed23fed187322c6c58cfa089f7396214bce416a
AraoSousa <aarao.sousa@estudante.ufjf.br>
2024-01-04 17:36:33 -0300

    criando novo arquivo de texto

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-
 console.log(textIn);
 
 
-
-
+const textOut = `This is what we know about avicado: ${textIn} \nCreated on ${Date.now()}`; // editar o texto de saída 
+fs.writeFileSync('./txt/output.txt', textOut); //cria um novo arquivo com o texto editado
+console.log('Fire Wireten!');
