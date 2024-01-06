da65a915c4d1b8d16504058b32dc06fe19bcd187
AraoSousa <aarao.sousa@estudante.ufjf.br>
2024-01-06 14:29:36 -0300

    Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+
+////////////////////////////////////////
+//FILES
 
 //blokking, modo síncrono
 //const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,17 +14,28 @@ const fs = require('fs');
 
 //no-blokking, modo assíncrono
 
-fs.readFile('./txt/start.txt','utf-8' , (_err, data1) => {
-  if(_err) return console.log('ERROR!!');
-    fs.readFile(`./txt/${data1}.txt`,'utf-8' , (_err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt','utf-8' , (_err, data3) => {
-            console.log(data3);
+// fs.readFile('./txt/start.txt','utf-8' , (_err, data1) => {
+//   if(_err) return console.log('ERROR!!');
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8' , (_err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt','utf-8' , (_err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}` , 'utf-8',err => {
-                console.log('arquivo criado');
-            });
-        });   
-    });
-});   
-console.log('leu aqui');
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}` , 'utf-8',err => {
+//                 console.log('arquivo criado');
+//             });
+//         });   
+//     });
+// });   
+// console.log('leu aqui');
+/////////////////////////////////////////
+
+
+//SERVER
+const server = http.createServer((req, res) => {   
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on the port 8000');
+})
